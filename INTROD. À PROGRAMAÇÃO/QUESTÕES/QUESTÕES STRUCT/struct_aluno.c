9e247a6798a954aaf92956a8434a35e80a5bcc62
Fábio Júnior <fabiojunioreloi@hotmail.com>
2023-12-15 12:11:01 -0300

    update

--- INTROD. À PROGRAMAÇÃO/QUESTÕES/QUESTÕES STRUCT/struct_aluno.c
+++ INTROD. À PROGRAMAÇÃO/QUESTÕES/QUESTÕES STRUCT/struct_aluno.c
@@ -2,50 +2,53 @@
 #include <locale.h>
 
 /*
-Fac¸a um programa que realize a leitura dos seguintes dados relativos a um conjunto de
-alunos: Matricula, Nome, Codigo da Disciplina, Nota1 e Nota2. Considere uma turma de ´
-ate 10 alunos. Ap ´ os ler todos os dados digitados, e depois de armazen ´ a-los em um vetor ´
-de estrutura, exibir na tela a listagem final dos alunos com as suas respectivas medias ´
+Facï¿½a um programa que realize a leitura dos seguintes dados relativos a um conjunto de
+alunos: Matricula, Nome, Codigo da Disciplina, Nota1 e Nota2. Considere uma turma de ï¿½
+ate 10 alunos. Ap ï¿½ os ler todos os dados digitados, e depois de armazen ï¿½ a-los em um vetor ï¿½
+de estrutura, exibir na tela a listagem final dos alunos com as suas respectivas medias ï¿½
 finais (use uma media ponderada: Nota1 com peso=1.0 e Nota2 com peso=2.0).
 */
 
 typedef struct{
 	char matricula[20], nome[30], codigo_Disciplina[15];
 	float nota1, nota2, media_aluno;
-}escola;
+}ESCOLA;
 
 
 int main(){
 	
 	setlocale(LC_ALL,"");
 	int qtd = 2, i;
-	escola alunos[qtd];
+	ESCOLA alunos[qtd];
 	float media;
 	
 	
 	for(i = 0; i < qtd; i++){
-		printf("%dº aluno: \n\n", i+1);
+		printf("%dÂº aluno: \n\n", i+1);
 		printf("Digite o nome: \n");
-		gets(alunos[i].nome);
-		printf("Digite a matrícula: \n");
-		gets(alunos[i].matricula);
-		printf("Digite o código da disciplina: \n");
-		gets(alunos[i].codigo_Disciplina);
-		fflush(stdin);
-		printf("Digite a 1ª nota: \n");
+		fgets(alunos[i].nome, sizeof(alunos[i].nome), stdin);
+		
+		printf("Digite a matrÃ­cula: \n");
+		fgets(alunos[i].matricula, sizeof(alunos[i].matricula), stdin);
+		
+		printf("Digite o cÃ³digo da disciplina: \n");
+		fgets(alunos[i].codigo_Disciplina, sizeof(alunos[i].codigo_Disciplina), stdin);
+		
+		printf("Digite a 1Âª nota: \n");
 		scanf("%f", &alunos[i].nota1);
-		fflush(stdin);
-		printf("Digite a 2ª nota: \n");
+		
+		printf("Digite a 2Âª nota: \n");
 		scanf("%f", &alunos[i].nota2);
-		fflush(stdin);	
+		while (getchar() != '\n');
+
 		alunos[i].media_aluno = (alunos[i].nota1 + (alunos[i].nota2 *2))/ 3;
 		printf("\n\n");	
 	}
 	
 	for(i = 0; i < qtd; i++){
-		printf("Resultado %dº aluno: \n\n", i+1);
-		printf("Nome: %s\nMatrícula: %s\nCódigo da Disciplina: %s\n", alunos[i].nome, alunos[i].matricula, alunos[i].codigo_Disciplina);
-		printf("Média: %.2f\n\n", alunos[i].media_aluno);
+		printf("Resultado %dÂº aluno: \n", i+1);
+		printf("Nome: %sMatrÃ­cula: %sCÃ³digo da Disciplina: %s", alunos[i].nome, alunos[i].matricula, alunos[i].codigo_Disciplina);
+		printf("MÃ©dia: %.2f\n\n", alunos[i].media_aluno);
 		
 	}
 	
